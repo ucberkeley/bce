bafb698e7b69d1cf689dc6ba3b95f86f6be7d811
Ryan Lovett <rylo@berkeley.edu>
2014-12-30 23:37:15 -0800

    Clarify comment regarding file provisioner.

--- provisioning/bootstrap-bce.sh
+++ provisioning/bootstrap-bce.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# These depend on bootstrap.d to be entered as a file provisioner in the json
+# This requires that bootstrap.d be entered as a file provisioner in
+# the packer json file.
 
 for s in /tmp/bootstrap.d/[0-9][0-9]-* ; do
 	echo "BCE: Sourcing bootstrap.d script '${s}'."
