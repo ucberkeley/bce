3a774276f443c7b7d2e39d22544e22c264b5fb71
Christopher Paciorek <paciorek@scf.Berkeley.EDU>
2015-03-13 17:19:49 -0700

    initial unfinished edits to add-parallel-tools.sh to go to openmpi 1.8.4

--- post-install/add-parallel-tools.sh
+++ post-install/add-parallel-tools.sh
@@ -10,17 +10,36 @@ export DEBIAN_PRIORITY=high
 export DEBIAN_FRONTEND=noninteractive
 
 # install fast linear algebra package and MPI functionality
+# we install openMPI from source because Ubuntu-packaged version (1.6.5)
+# has bug in MPI_Comm_spawn affecting Rmpi
+git config --global user.email "bce@lists.berkeley.edu"
+git config --global user.name "BCE Release Team"
+
 apt-get update
-apt-get install -y libopenblas-base libopenmpi-dev openmpi-bin
+apt-get install -y libopenblas-base # libopenmpi-dev openmpi-bin
+
+MPI_VERSION=1.8.4
+MPI_MAJOR_VERSION=$(echo $MPI_VERSION | cut -d"." -f1,2)
+
+mkdir /usr/local/openmpi-${MPI_VERSION}
+ln -s /usr/local/openmpi-${MPI_VERSION} /usr/local/openmpi
+cd /usr/local/openmpi
+wget http://www.stat.berkeley.edu/~paciorek/transfer/bce-openmpi-${MPI_VERSION}.tar.gz
+tar -xvzf bce-openmpi-${MPI_VERSION}.tar.gz
+rm bce-openmpi-${MPI_VERSION}.tar.gz
+echo /usr/local/openmpi/lib > /etc/ld.so.conf.d/openmpi.conf
+ldconfig
+echo "export PATH=${PATH}:/usr/local/openmpi/bin" >> /etc/profile
+export PATH=${PATH}:/usr/local/openmpi/bin
 
 # install parallelization packages for Python
 HOME=/root pip install multiprocessing mpi4py
 
-ppVersion=1.6.4
+PP_VERSION=1.6.4
 cd /tmp
-wget http://www.parallelpython.com/downloads/pp/pp-${ppVersion}.tar.gz
-tar -xvzf pp-${ppVersion}.tar.gz
-cd pp-${ppVersion}
+wget http://www.parallelpython.com/downloads/pp/pp-${PP_VERSION}.tar.gz
+tar -xvzf pp-${PP_VERSION}.tar.gz
+cd pp-${PP_VERSION}
 python setup.py install
 
 # install parallelization packages for R
