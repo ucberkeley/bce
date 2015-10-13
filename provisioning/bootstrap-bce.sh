3e718dadac160a16e42572162371faf7246ce5ec
Carl Boettiger <cboettig@gmail.com>
2015-10-13 20:48:50 +0000

    Dockerized version of Fall-2015 BCE image

--- provisioning/bootstrap-bce.sh
+++ provisioning/bootstrap-bce.sh
@@ -3,6 +3,9 @@
 # This requires that bootstrap.d be entered as a file provisioner in
 # the packer json file.
 
+## Import debconf.selections for automatic building (rather than passing as kernel boot parameters)
+debconf-set-selections < debconf.selections
+
 for s in /tmp/bootstrap.d/[0-9][0-9]-* ; do
 	echo "BCE: Sourcing bootstrap.d script '${s}'."
 	. $s
