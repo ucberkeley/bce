57780fbaf2fc80bff9c16e12589b56c923307ca8
Carl Boettiger <cboettig@gmail.com>
2015-10-14 16:43:02 +0000

    avoid modifying bootstrap-bce.sh

--- provisioning/bootstrap-bce.sh
+++ provisioning/bootstrap-bce.sh
@@ -3,10 +3,6 @@
 # This requires that bootstrap.d be entered as a file provisioner in
 # the packer json file.
 
-## Import debconf.selections for automatic building (rather than passing as kernel boot parameters)
-## Apparently running this command in the Dockerfile instead of here has no effect..
-#debconf-set-selections < debconf.selections
-
 for s in /tmp/bootstrap.d/[0-9][0-9]-* ; do
 	echo "BCE: Sourcing bootstrap.d script '${s}'."
 	. $s
